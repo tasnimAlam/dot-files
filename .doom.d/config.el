d60b63be7aa77ebd379b4b86c588fdf70c6bc815
Tasnim Alam <tasnimalamcse@gmail.com>
2020-04-24 15:59:53 +0600

    Update doom config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -3,27 +3,15 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
-
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "Tasnim Alam"
       user-mail-address "tasnimalamcse@gmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
+;; Font string. You generally only need these two:
 (setq doom-font (font-spec :family "Monaco" :size 16))
 
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
+;; Theme
 (setq doom-theme 'doom-one)
 
 ;; If you use `org' and don't want your org files in the default location below,
@@ -34,26 +22,22 @@
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type 'relative)
 
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
-;; they are implemented.
+;; Start doom fullscreen mode
 (custom-set-variables
  '(initial-frame-alist (quote ((fullscreen . maximized)))))
-(setq-default evil-escape-key-sequence "kj")
 
-(add-hook 'js2-mode-hook 'prettier-js-mode)
-(add-hook 'web-mode-hook 'prettier-js-mode)
+;; Custom key bindings
+(setq-default evil-escape-key-sequence "kj")
+(setq avy-all-windows t)
+
+;; Tide config
+(after! js2-mode
+  (set-company-backend! 'js2-mode
+    '(company-files :with company-tide company-yasnippet)))
+(add-hook 'js2-mode-hook #'setup-tide-mode)
+(setq company-minimum-prefix-length 1
+      company-idle-delay 0.0)
+
+;; Prettier config
+ (add-hook 'js2-mode-hook 'prettier-js-mode)
+ (add-hook 'web-mode-hook 'prettier-js-mode)
