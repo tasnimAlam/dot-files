e909603ae9a1deda7164d8b6c5ac4c9b21d26b43
Md. Tasnim Alam <tasnimalamcse@gmail.com>
2021-10-15 21:25:59 +0600

    chore: add installation file

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Add new config folder here 
+config_folders=("bspwm" "dunst" "fish" "kitty" "nvim" "picom" "polybar" "rofi" "sxhkd" "zathura" "starship.toml" )
+base_dir="${HOME}/.config/"
+
+for folder in "${config_folders[@]}";do
+  fd $folder -d1 $base_dir -x cp -r {} ./.config
+done;
