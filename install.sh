249544a8946b1da7817e43047088422ef78f8b58
Md. Tasnim Alam <tasnimalamcse@gmail.com>
2021-10-15 21:38:43 +0600

    chore: clean code

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@
 config_folders=("bspwm" "dunst" "fish" "kitty" "nvim" "picom" "polybar" "rofi" "sxhkd" "zathura" "starship.toml" )
 base_dir="${HOME}/.config/"
 
-for folder in "${config_folders[@]}";do
+for folder in ${config_folders[@]};do
   fd $folder -d1 $base_dir -x cp -r {} ./.config
 done;
