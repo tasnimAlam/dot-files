80e78ad28f8687e36b58578bd539680472b971c4
Md. Tasnim Alam <tasnimalamcse@gmail.com>
2022-01-04 12:22:22 +0600

    nvim: hide virtual text

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -68,3 +68,12 @@ for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = ""})
 end
+
+-- Hide virtual texty
+vim.diagnostic.config(
+  {
+    virtual_text = false,
+    signs = true,
+    float = {border = "single"}
+  }
+)
