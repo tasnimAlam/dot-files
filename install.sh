348bcbb73971d7749c94d6c8f8cb51ef5274d1b8
tasnimAlam <tasnimalamcse@gmail.com>
2022-04-02 23:58:22 +0600

    update install script

--- install.sh
+++ install.sh
@@ -3,13 +3,22 @@
 # Install packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
+# Install packages
+sudo pacman -S --noconfirm --needed - <packages.txt
+yay -S --noconfirm fnm mycli slack visual-studio-code-bin
+
+# install fisher
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+
 # Add new config folder here
 config_folders=("bspwm" "dunst" "fish" "kitty" "nvim" "picom" "polybar" "rofi" "sxhkd" "zathura" "starship.toml")
 base_dir="${HOME}/.config/"
 
 for folder in ${config_folders[@]}; do
-  fd $folder -d1 $base_dir -x cp -r {} ./.config
+	fd $folder -d1 $base_dir -x cp -r {} ./.config
 done
 
 # Remove compiled pakcer file
 rm ./.config/nvim/packer/packer.nvim
+
+fish
