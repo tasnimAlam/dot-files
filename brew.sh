31aef91672822ff9ac1a34160da1c3cbe29a65b2
tasnimAlam <tasnimalamcse@gmail.com>
2023-01-25 16:32:48 +0600

    chore: update install files

---
+++ brew.sh
@@ -0,0 +1,2 @@
+brew leaves >brew.txt
+xargs brew install <brew.txt
--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 # Install packages
-sudo pacman -S --noconfirm --needed - <packages.txt
+sudo pacman -S --noconfirm --needed - <pacman.txt
 yay -S --noconfirm - <yay.txt
 
 # Npm packages
