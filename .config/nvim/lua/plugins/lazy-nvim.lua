86c34de287e4b9a289ca2d559568f02e7a988716
tasnimAlam <tasnimalamcse@gmail.com>
2023-06-28 17:32:52 +0600

    nvim: add flash.nvim plugin

--- .config/nvim/lua/plugins/lazy-nvim.lua
+++ .config/nvim/lua/plugins/lazy-nvim.lua
@@ -324,6 +324,39 @@ require("lazy").setup({
 		end,
 	},
 	{ "tasnimAlam/px2rem.lua" },
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {},
+		keys = {
+			{
+				"s",
+				mode = { "n", "x", "o" },
+				function()
+					-- default options: exact mode, multi window, all directions, with a backdrop
+					require("flash").jump()
+				end,
+				desc = "Flash",
+			},
+			{
+				"S",
+				mode = { "n", "o", "x" },
+				function()
+					require("flash").treesitter()
+				end,
+				desc = "Flash Treesitter",
+			},
+			{
+				"r",
+				mode = "o",
+				function()
+					require("flash").remote()
+				end,
+				desc = "Remote Flash",
+			},
+		},
+	},
 
 	-- Rest API
 	{
