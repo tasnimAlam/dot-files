aab439c74a92d95a0edab2f06e35cd9f34059333
tasnimStrativ <tasnim@strativ.se>
2024-05-18 01:24:48 +0600

    fish: add fifc plugin

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,15 +9,17 @@ set -gx LC_ALL en_US.UTF-8
 set -gx DMENU_BLUETOOTH_LAUNCHER dmenu-wl
 
 
-# Docker 
-# set -gx DOCKER_HOST unix://$XDG_RUNTIME_DIR/docker.sock
-
-set fish_key_bindings fish_user_key_bindings
-
 # Fzf config
+
+set -Ux fifc_editor nvim
+set fish_key_bindings fish_user_key_bindings
+fzf_configure_bindings --variables=\e\cv
+
 set fzf_preview_file_cmd cat
 set fzf_preview_dir_cmd exa --all --color=always
 set fzf_fd_opts --hidden --exclude=.git
+set fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
+
 set -x FZF_DEFAULT_OPTS "--reverse --bind 'ctrl-y:execute-silent(printf {} | cut -f 2- | wl-copy --trim-newline)'"
 set -gx FZF_DEFAULT_COMMAND "fd --type f --strip-cwd-prefix"
 set -gx FZF_CTRL_T_OPTS " --walker-skip .git,node_modules,target --preview 'bat -n --color=always {}' --bind 'ctrl-/:change-preview-window(down|hidden|)'"
@@ -27,8 +29,8 @@ set -gx FZF_ALT_C_OPTS " --walker-skip .git,node_modules,target --preview 'tree 
 fish_add_path ~/bin
 fish_add_path /usr/local/bin /usr/local/sbin
 fish_add_path ~/.local/bin/
-fish_add_path ~/.emacs.d/bin/
-fish_add_path ~/.config/emacs/bin
+fish_add_path ~/.config/emacs/bin/
+fish_add_path ~/.config/doom//bin/
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.yarn/bin ~/.config/yarn/global/node_modules/.bin ~/.npm-global/bin
 fish_add_path ~/.composer/vendor/bin
@@ -42,7 +44,6 @@ if test "$os" = Darwin
     fish_add_path /opt/homebrew/bin /opt/homebrew/sbin
     fish_add_path ~/Projects/scripts/
 end
-
 
 
 # NNN config
@@ -64,13 +65,13 @@ set SOCK 03
 set UNKNOWN 01
 set -x NNN_COLORS "#04020301;4231"
 set -x NNN_FCOLORS "$BLK$CHR$DIR$EXE$REG$HARDLINK$SYMLINK$MISSING$ORPHAN$FIFO$SOCK$UNKNOWN"
-set -x NNN_PREFER_SELECTION 1
+# set -x NNN_PREFER_SELECTION 1
 
 set -x CHROME_BIN /usr/bin/chromium
 
 
 if test "$os" = Linux
-    set -x NNN_BMS "d:~/Downloads/;w:~/Documents/sports-cloud-webapp;u:~/Documents/ui2/;r:~/Documents/rust-projects/rust_test/;.:~/Documents/dot-files/;p:~/Pictures/"
+    set -x NNN_BMS "d:~/Downloads/;w:~/Documents/sports-cloud-webapp;u:~/Documents/ui2/;r:~/Documents/rust-projects/rust_test/;.:~/Documents/dot-files/;p:~/Pictures/;v:~/Videos/tutorials/"
 end
 
 if test "$os" = Darwin
