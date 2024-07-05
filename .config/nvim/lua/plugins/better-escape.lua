a4e4720d22a3a89b6c0a79193686dc651966f6e8
tasnimAlam <tasnimalamcse@gmail.com>
2024-07-05 15:03:32 +0600

    nvim: fix better escape breaking changes

--- .config/nvim/lua/plugins/better-escape.lua
+++ .config/nvim/lua/plugins/better-escape.lua
@@ -1,6 +1,11 @@
-require("better_escape").setup {
-  mapping = {"kj"},
-  timeout = vim.o.timeoutlen,
-  clear_empty_lines = false,
-  keys = "<Esc>"
-}
+require("better_escape").setup({
+	timeout = vim.o.timeoutlen,
+	mappings = {
+		i = {
+			k = {
+				k = "<Esc>",
+				j = "<Esc>",
+			},
+		},
+	},
+})
