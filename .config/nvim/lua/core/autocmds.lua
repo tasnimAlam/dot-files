d7c7da18fd2eafe158f9974386de3c512e548e97
tasnimAlam <tasnimalamcse@gmail.com>
2024-08-17 02:17:23 +0600

    nvim: add hyprls

--- .config/nvim/lua/core/autocmds.lua
+++ .config/nvim/lua/core/autocmds.lua
@@ -14,3 +14,21 @@ vim.cmd([[
 vim.cmd([[
   autocmd CmdWinLeave * lua require('cmp').setup({enabled = true})
 ]])
+
+-- Enable hyprland treesitter
+vim.filetype.add({
+	pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
+})
+
+-- Hyprlang LSP
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+	pattern = { "*.hl", "hypr*.conf" },
+	callback = function(event)
+		print(string.format("starting hyprls for %s", vim.inspect(event)))
+		vim.lsp.start({
+			name = "hyprlang",
+			cmd = { "hyprls" },
+			root_dir = vim.fn.getcwd(),
+		})
+	end,
+})
