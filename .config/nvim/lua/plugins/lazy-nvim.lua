11e94136d7dac1abaa8223b3c808fabe7fd1a49c
tasnimStrativ <tasnim@strativ.se>
2025-02-11 18:06:00 +0600

    nvim: update symbol find keybind

--- .config/nvim/lua/plugins/lazy-nvim.lua
+++ .config/nvim/lua/plugins/lazy-nvim.lua
@@ -517,7 +517,38 @@ require("lazy").setup({
 		---@type snacks.Config
 		opts = {
 			bigfile = { enabled = true },
-			picker = {},
+			picker = {
+				win = {
+					input = {
+						keys = {
+							["<a-s>"] = { "flash", mode = { "n", "i" } },
+							["s"] = { "flash" },
+							["<S-k>"] = { "preview_scroll_up", mode = { "i", "n" } },
+							["<S-j>"] = { "preview_scroll_down", mode = { "i", "n" } },
+						},
+					},
+				},
+				actions = {
+					flash = function(picker)
+						require("flash").jump({
+							pattern = "^",
+							label = { after = { 0, 0 } },
+							search = {
+								mode = "search",
+								exclude = {
+									function(win)
+										return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "snacks_picker_list"
+									end,
+								},
+							},
+							action = function(match)
+								local idx = picker.list:row2idx(match.pos[1])
+								picker.list:_move(idx, true, true)
+							end,
+						})
+					end,
+				},
+			},
 			dashboard = {
 				enabled = true,
 				sections = {
@@ -716,7 +747,7 @@ require("lazy").setup({
 				desc = "Goto T[y]pe Definition",
 			},
 			{
-				"gs",
+				"<Leader>fs",
 				function()
 					Snacks.picker.lsp_symbols()
 				end,
