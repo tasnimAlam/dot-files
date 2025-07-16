e176b4928f6341001da19986c3c554253b6f425d
tasnimAlam <tasnimalamcse@gmail.com>
2025-07-16 16:33:51 +0600

    yazi: update config

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,18 +1,3 @@
--- ~/.config/yazi/init.lua
-require("bookmarks"):setup({
-	last_directory = { enable = false, persist = true },
-	persist = "all",
-	desc_format = "full",
-	file_pick_mode = "hover",
-	notify = {
-		enable = false,
-		timeout = 1,
-		message = {
-			new = "New bookmark '<key>' -> '<folder>'",
-			delete = "Deleted bookmark in '<key>'",
-			delete_all = "Deleted all bookmarks",
-		},
-	},
-})
-
 require("folder-rules"):setup()
+
+require("relative-motions"):setup({ show_numbers = "relative", show_motion = true, enter_mode = "first" })
