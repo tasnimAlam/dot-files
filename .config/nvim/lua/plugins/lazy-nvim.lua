19aa571e5174b191afac106a3b0321d3066ae22e
tasnimAlam <tasnimalamcse@gmail.com>
2025-09-23 01:20:25 +0600

    nvim: add mini clue

--- .config/nvim/lua/plugins/lazy-nvim.lua
+++ .config/nvim/lua/plugins/lazy-nvim.lua
@@ -960,4 +960,52 @@ require("lazy").setup({
 			},
 		},
 	},
+	{
+		"nvim-mini/mini.clue",
+		version = "*",
+		config = function()
+			require("mini.clue").setup({
+				triggers = {
+					-- Leader triggers
+					{ mode = 'n', keys = '<Leader>' },
+					{ mode = 'x', keys = '<Leader>' },
+
+					-- Built-in completion
+					{ mode = 'i', keys = '<C-x>' },
+
+					-- `g` key
+					{ mode = 'n', keys = 'g' },
+					{ mode = 'x', keys = 'g' },
+
+					-- Marks
+					{ mode = 'n', keys = "'" },
+					{ mode = 'n', keys = '`' },
+					{ mode = 'x', keys = "'" },
+					{ mode = 'x', keys = '`' },
+
+					-- Registers
+					{ mode = 'n', keys = '"' },
+					{ mode = 'x', keys = '"' },
+					{ mode = 'i', keys = '<C-r>' },
+					{ mode = 'c', keys = '<C-r>' },
+
+					-- Window commands
+					{ mode = 'n', keys = '<C-w>' },
+
+					-- `z` key
+					{ mode = 'n', keys = 'z' },
+					{ mode = 'x', keys = 'z' },
+				},
+
+				clues = {
+					require('mini.clue').gen_clues.builtin_completion(),
+					require('mini.clue').gen_clues.g(),
+					require('mini.clue').gen_clues.marks(),
+					require('mini.clue').gen_clues.registers(),
+					require('mini.clue').gen_clues.windows(),
+					require('mini.clue').gen_clues.z(),
+				},
+			})
+		end,
+	},
 })
