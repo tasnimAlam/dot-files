afb6e31b2054a943029c10ea622bdb06bfb83a62
tasnimAlam <tasnimalamcse@gmail.com>
2026-05-04 17:48:12 +0600

    yazi: add git plugin

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,2 +1,6 @@
 require("folder-rules"):setup()
 require("relative-motions"):setup({ show_numbers = "relative", show_motion = true, enter_mode = "first" })
+require("git"):setup({
+	-- Order of status signs showing in the linemode
+	order = 1500,
+})
