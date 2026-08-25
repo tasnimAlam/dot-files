5709f9b885558cbc9e4d1cc560b417b2a245c70b
tasnimAlam <tasnimalamcse@gmail.com>
2026-08-25 13:55:43 +0600

    yazi: remove relative motions plugin

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,5 +1,4 @@
 require("folder-rules"):setup()
-require("relative-motions"):setup({ show_numbers = "relative", show_motion = true, enter_mode = "first" })
 require("git"):setup({
 	-- Order of status signs showing in the linemode
 	order = 1500,
